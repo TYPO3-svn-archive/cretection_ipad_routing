343b6f3dd0025336ea9db885fae2d43e11b2c355
cretection <cretection@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-01 12:39:46 +0000

    First
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cretection_ipad_routing/trunk@35207 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "cretection_ipad_routing".
+#
+# Auto generated 01-07-2010 14:26
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'iPad Routing',
+	'description' => 'When a iPad opens your website this Extension will lead it to a chosen site.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '1.0.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jonathan Starck',
+	'author_email' => 'info@cretection.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"aef5";s:17:"ext_localconf.php";s:4:"c9da";s:14:"ext_tables.php";s:4:"cc32";s:14:"ext_tables.sql";s:4:"9f36";s:16:"locallang_db.xml";s:4:"d9e1";s:14:"doc/manual.sxw";s:4:"422b";s:44:"pi1/class.tx_cretectionipadrouting_pi1.php";s:4:"3b46";s:31:"static/ipad_routing/setup.txt";s:4:"3e80";}',
+);
+
+?>
